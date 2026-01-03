be64e1812b7405381bdec29495dd410c27273ee6
Mounir <mounir.arrahmani@outlook.com>
2026-01-03 18:17:43 +0100

    wip

--- apps/api/database/mock/theme.ts
+++ apps/api/database/mock/theme.ts
@@ -10,10 +10,10 @@ export const BRAND_COLORS = {
 export const BRAND_ASSETS = {
     // Assets are located in apps/api/database/mock/assets/
     // Please upload them and update these links:
-    logo: 'https://raw.githubusercontent.com/Mouness/fundraising-event/10c982dfeec35ed87d29b448250fc674c2ddf3dc/apps/api/database/mock/assets/logo.png',
-    logoIcon: 'https://raw.githubusercontent.com/Mouness/fundraising-event/10c982dfeec35ed87d29b448250fc674c2ddf3dc/apps/api/database/mock/assets/logo_icon.png',
-    favicon: 'https://raw.githubusercontent.com/Mouness/fundraising-event/10c982dfeec35ed87d29b448250fc674c2ddf3dc/apps/api/database/mock/assets/favicon.png',
+    logo: 'https://raw.githubusercontent.com/Mouness/fundraising-event/63ee5cbc391ec97283429c7f8a10f83bd0c90c96/apps/api/database/mock/assets/logo.png',
+    logoIcon: 'https://raw.githubusercontent.com/Mouness/fundraising-event/63ee5cbc391ec97283429c7f8a10f83bd0c90c96/apps/api/database/mock/assets/logo_icon.png',
+    favicon: 'https://raw.githubusercontent.com/Mouness/fundraising-event/63ee5cbc391ec97283429c7f8a10f83bd0c90c96/apps/api/database/mock/assets/favicon.png',
     bgLanding: 'https://raw.githubusercontent.com/Mouness/fundraising-event/10c982dfeec35ed87d29b448250fc674c2ddf3dc/apps/api/database/mock/assets/bg_landing.png',
     bgDonor: 'https://raw.githubusercontent.com/Mouness/fundraising-event/10c982dfeec35ed87d29b448250fc674c2ddf3dc/apps/api/database/mock/assets/bg_donor.png',
-    bgLive: 'https://raw.githubusercontent.com/Mouness/fundraising-event/10c982dfeec35ed87d29b448250fc674c2ddf3dc/apps/api/database/mock/assets/bg_live.png'
+    bgLive: 'https://raw.githubusercontent.com/Mouness/fundraising-event/3285570e77755dbd8103066dbd24fa7c6c82dbde/apps/api/database/mock/assets/bg_live_clean.png'
 };
